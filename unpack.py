2d1c9e04d169e473836e6e36c89944c000d3a3a5
shreya643 <shreya998.ss@gmail.com>
2017-12-17 22:32:42 +0545

    unpack.py commited

---
+++ unpack.py
@@ -0,0 +1,6 @@
+#Q5. Write a function that simply returns ("Hello", 45, 23.3).
+# Now, call this function and unpack the returned values and print it.
+
+def f(call):
+    return ("Hello", 45, 23.3)
+
